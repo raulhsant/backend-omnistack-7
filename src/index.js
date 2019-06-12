8fb1bffc19e45f7eca3ad079839c6ef7c1df62c6
Raul <raulhsantana@gmail.com>
2019-06-12 01:51:18 -0300

    create routes and Post model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express =  require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send(`Hello world ${req.query.name}!!`);
+mongoose.connect('mongodb+srv://semana:O1hhKgBnOguIgWz0@cluster0-0biwa.mongodb.net/test?retryWrites=true&w=majority', { 
+    useNewUrlParser: true,
 })
+
+app.use(require('./routes'));
 
 app.listen(9003)
